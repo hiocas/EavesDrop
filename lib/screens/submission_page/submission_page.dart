9c8f77a9476f801bf0a72e292bb5762a86965e30
hiocas <ew0nd3r123@gmail.com>
2021-06-29 16:52:58 +0300

    Removed the RefreshIndicator from SubmissionPage as it serves little use and causes problems.

--- lib/screens/submission_page/submission_page.dart
+++ lib/screens/submission_page/submission_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:gwa_app/models/hive_boxes.dart';
-import 'package:gwa_app/utils/util_functions.dart';
 import 'package:gwa_app/widgets/markdown_viewer.dart';
 import 'package:gwa_app/models/gwa_submission.dart';
 import 'local_widgets/all_page_local.dart';
@@ -78,94 +77,81 @@ class SubmissionPageState extends State<SubmissionPage> {
           );
         } else {
           _submission = new GwaSubmission(snapshot.data);
-          return RefreshIndicator(
-            color: Theme.of(context).accentColor,
-            backgroundColor: Theme.of(context).backgroundColor,
-            /*TODO: Implement pull to refresh without pushing a new route (it
-                looks confusing and a bit jarring). */
-            onRefresh: () {
-              /*FIXME: This makes it so we can't search using tags after
-                 refreshing. */
-              pushReplacementSubmissionPageWithReturnData(
-                  context, widget.submissionFullname, widget.fromLibrary);
-              return Future.value();
-            },
-            child: Scaffold(
-              backgroundColor: Theme.of(context).primaryColor,
-              body: SafeArea(
-                child: Container(
-                  color: Theme.of(context).backgroundColor,
-                  child: CustomScrollView(
-                    controller: _scrollController,
-                    physics: const BouncingScrollPhysics(
-                        parent: AlwaysScrollableScrollPhysics()),
-                    slivers: [
-                      //App Bar
-                      SubmissionPageAppBar(
+          return Scaffold(
+            backgroundColor: Theme.of(context).primaryColor,
+            body: SafeArea(
+              child: Container(
+                color: Theme.of(context).backgroundColor,
+                child: CustomScrollView(
+                  controller: _scrollController,
+                  physics: const BouncingScrollPhysics(
+                      parent: AlwaysScrollableScrollPhysics()),
+                  slivers: [
+                    //App Bar
+                    SubmissionPageAppBar(
+                      submission: _submission,
+                      maxTitleTop: 120,
+                      maxTitleAlignTop: 200,
+                    ),
+                    //Buttons and Tags
+                    SubmissionPageButtonsAndTags(
+                        mini: miniButtons,
                         submission: _submission,
-                        maxTitleTop: 120,
-                        maxTitleAlignTop: 200,
-                      ),
-                      //Buttons and Tags
-                      SubmissionPageButtonsAndTags(
-                          mini: miniButtons,
-                          submission: _submission,
-                          redditSubmission: snapshot.data,
-                          fromLibrary: widget.fromLibrary),
-                      //MarkdownViewer
-                      SliverPadding(
-                        padding: const EdgeInsets.all(10.0),
-                        sliver: SliverToBoxAdapter(
-                            child: GestureDetector(
-                          behavior: HitTestBehavior.deferToChild,
-                          onTap: () {
-                            print('Hide button');
-                            if (_floatingPlayButtonKey.currentState.animates) {
-                              _floatingPlayButtonKey.currentState
-                                  .animateButton();
-                            }
-                          },
-                          child: Material(
-                            color: Theme.of(context).backgroundColor,
-                            elevation: 15.0,
-                            shape: RoundedRectangleBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(32.0))),
-                            child: Padding(
-                              padding: const EdgeInsets.all(14.0),
-                              child: MarkdownViewer(
-                                text: _submission.selftext,
-                                fromLibrary: widget.fromLibrary,
-                                inPopupCard: false,
-                                bodyTextFontSize: 14.0,
-                              ),
+                        redditSubmission: snapshot.data,
+                        fromLibrary: widget.fromLibrary),
+                    //MarkdownViewer
+                    SliverPadding(
+                      padding: const EdgeInsets.all(10.0),
+                      sliver: SliverToBoxAdapter(
+                          child: GestureDetector(
+                        behavior: HitTestBehavior.deferToChild,
+                        onTap: () {
+                          print('Hide button');
+                          if (_floatingPlayButtonKey.currentState.animates) {
+                            _floatingPlayButtonKey.currentState
+                                .animateButton();
+                          }
+                        },
+                        child: Material(
+                          color: Theme.of(context).backgroundColor,
+                          elevation: 15.0,
+                          shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(32.0))),
+                          child: Padding(
+                            padding: const EdgeInsets.all(14.0),
+                            child: MarkdownViewer(
+                              text: _submission.selftext,
+                              fromLibrary: widget.fromLibrary,
+                              inPopupCard: false,
+                              bodyTextFontSize: 14.0,
                             ),
                           ),
-                        )),
-                      ),
-                      /*FIXME(Design): This makes sure the SelfTextViewer (now the
-                          MarkdownViewer) can be fully read without the floating
-                          action button blocking the text at the end of the
-                          CustomScrollView. Find a better solution or redesign
-                          the ui. */
-                      SliverToBoxAdapter(
-                        child: Container(
-                          height: 70,
                         ),
-                      )
-                    ],
-                  ),
+                      )),
+                    ),
+                    /*FIXME(Design): This makes sure the SelfTextViewer (now the
+                        MarkdownViewer) can be fully read without the floating
+                        action button blocking the text at the end of the
+                        CustomScrollView. Find a better solution or redesign
+                        the ui. */
+                    SliverToBoxAdapter(
+                      child: Container(
+                        height: 70,
+                      ),
+                    )
+                  ],
                 ),
               ),
-              floatingActionButton: FloatingPlayButton(
-                key: _floatingPlayButtonKey,
-                heroTag: 'floating-play-button-popup',
-                submission: _submission,
-                scrollController: _scrollController,
-              ),
-              floatingActionButtonLocation:
-                  FloatingActionButtonLocation.centerFloat,
             ),
+            floatingActionButton: FloatingPlayButton(
+              key: _floatingPlayButtonKey,
+              heroTag: 'floating-play-button-popup',
+              submission: _submission,
+              scrollController: _scrollController,
+            ),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
           );
         }
       },
